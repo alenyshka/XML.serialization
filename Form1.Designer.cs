2c2b48d5301a7120486a03db536d6aff84675b2f
alenyshka <lena_borikov@mail.ru>
2016-05-24 21:04:40 +0300

    Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace XML.serializaton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Font = new System.Drawing.Font("Proxy 1", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(463, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Serialization";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.Font = new System.Drawing.Font("Proxy 1", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(463, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Deserialization";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Earrings",
+            "Ring",
+            "Chain",
+            "Coulomb ",
+            "Wathes",
+            "Pin"});
+            this.comboBox1.Location = new System.Drawing.Point(505, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(495, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(498, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(498, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Weight:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(495, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Material:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(497, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Property:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(497, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Property:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(498, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(500, 172);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(500, 212);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(146, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(463, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(445, 329);
+            this.listBox1.TabIndex = 15;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(12, 347);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(445, 108);
+            this.textBoxInfo.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(463, 283);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(213, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(463, 312);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(213, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Edit";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(541, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "comment";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(688, 461);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        public void PutLabels()
+        {
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            textBox5.Visible = true;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = false;
+
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    label6.Visible = true;
+                    label4.Text = "Form:";
+                    label5.Text = "Gems:";
+                    label6.Text = "enter true or false";
+                    break;
+                case 1:
+                    label6.Visible = true;
+                    label4.Text = "Type:";
+                    label5.Text = "Gems:";
+                    label6.Text = "enter true or false";
+                    break;
+                case 2:
+                    label6.Visible = true;
+                    label4.Text = "Lenght:";
+                    label5.Text = "Clasp:";
+                    label6.Text = "enter true or false";
+                    break;
+                case 3:
+                    label6.Visible = true;
+                    label4.Text = "Form:";
+                    label5.Text = "Gems:";
+                    label6.Text = "enter true or false";
+                    break;
+                case 4:
+                    label4.Text = "mechanism:";
+                    label5.Text = "Type:";
+                    break;
+                case 5:
+                    label4.Text = "Type:";
+                    label5.Text = "Design:";
+                    break;
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+using XML.serializaton.Factories;
+using XML.serializaton.Decorations;
+
+namespace XML.serializaton
+{
+    public partial class Form1 : Form
+    {
+        Factory[] factory = new Factory[6];
+        List<DecorationClass> DecorationList = new List<DecorationClass>();
+        Type[] extraTypes = new Type[6];
+        List<string> FieldList = new List<string>();
+        List<TextBox> TextBoxList = new List<TextBox>();
+        int i;
+        bool flagEdit = false, flagDelete = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFabrika();
+            InitializaTypes();  
+           
+            TextBoxList.Add(textBox1);
+            TextBoxList.Add(textBox2);
+            TextBoxList.Add(textBox3);
+            TextBoxList.Add(textBox4);
+            TextBoxList.Add(textBox5);
+        }
+        void InitializeFabrika()
+        {
+            factory[0] = new EarringsFactory();
+            factory[1] = new RingFactory();
+            factory[2] = new ChainFactory();
+            factory[3] = new CoulombFactory();
+            factory[4] = new WatchesFactory();
+            factory[5] = new PinFactory();
+        }
+        void InitializaTypes()
+        {
+            extraTypes[0] = typeof(Earrings);
+            extraTypes[1] = typeof(Ring);
+            extraTypes[2] = typeof(Chain);
+            extraTypes[3] = typeof(Coulomb);
+            extraTypes[4] = typeof(Watches);
+            extraTypes[5] = typeof(Pin);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((DecorationList.Count > 0) /*&& (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)*/)
+                {
+                    //listBox1.Items.Clear();
+                    XmlSerializer mySerializer = new XmlSerializer(typeof(List<DecorationClass>), extraTypes);
+                    //System.IO.StreamReader reader = new System.IO.StreamReader(openFileDialog1.FileName);
+
+                    using (FileStream file = new FileStream("decoration.xml", FileMode.Create))
+                    {
+                        mySerializer.Serialize(file, DecorationList);
+                        file.Close();
+                    }
+                    textBoxInfo.Text = "Serialization completed successfully.\r\n";
+                }
+                else
+                    textBoxInfo.Text = "The list doesn't contain objects\r\n";
+            }
+            catch
+            {
+                textBoxInfo.Text = "Check the object(s) in list\r\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                FileStream fs = new FileStream("decoration.xml", FileMode.Open);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<DecorationClass>), extraTypes);
+
+                List<DecorationClass> Decoration = (List<DecorationClass>)xmlSerializer.Deserialize(fs);
+
+                foreach (DecorationClass element in Decoration)
+                {
+                    listBox1.Items.Add("Object = " + element.Object + "   Name = " +   element.Name + "   Weight = " + element.Weight + "   Material = " + element.Material);
+                    DecorationList.Add(element);
+                }
+                fs.Close();
+
+                textBoxInfo.Text = "Deserialization completed successfully\r\n";
+            }
+            
+            catch
+            {
+                textBoxInfo.Text = "Check the file decoration.xml\r\n";
+            }  
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PutLabels();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FieldList.Clear();
+                if(CheckInput())
+                {
+                    FieldList.Add(comboBox1.GetItemText(comboBox1.SelectedItem));
+                    for (i = 1; i <= TextBoxList.Count; i++)
+                       if (TextBoxList[i - 1].Text != "")
+                            FieldList.Add(TextBoxList[i - 1].Text);
+                    DecorationClass decoration = factory[comboBox1.SelectedIndex].FactoryMethod();
+                    decoration.SetValues(FieldList);
+                    DecorationList.Add(decoration);
+                    listBox1.Items.Add("Object = " + decoration.Object + "   Name = " + decoration.Name + "   Weight = " + +decoration.Weight + "   Material = " + decoration.Material);
+                    textBoxInfo.Text = "The object added.\r\n";
+                }
+
+            }
+            catch 
+            {
+                textBoxInfo.Text = "The object isn't added.\r\n";
+            }
+        }
+        bool CheckInput()
+        {
+            if ((textBox1.Text != "") && (textBox2.Text != "") && (textBox3.Text != "") && (textBox4.Text != "") && (textBox5.Text != ""))
+                return true;
+            else
+            {
+                textBoxInfo.Text = "Uncorrected input.";
+                return false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedIndex != -1)
+                {
+                    DecorationList.Remove(DecorationList[listBox1.SelectedIndex]);
+                    flagDelete = true;
+                    listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                    flagDelete = false;
+                    textBoxInfo.Text = "Object deleted.\r\n";
+                }
+                else
+                    textBoxInfo.Text = "Choose the object.\r\n";
+            }
+            catch
+            {
+                textBoxInfo.Text = "Choose the object.\r\n";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                FieldList.Clear();
+                if ((!flagEdit) && (!flagDelete))
+                {
+                    comboBox1.Text = DecorationList[listBox1.SelectedIndex].Object;
+                    DecorationList[listBox1.SelectedIndex].GetValues(FieldList);
+                    for (i = 0; i < FieldList.Count-1; i++)
+                        TextBoxList[i].Text = FieldList[i+1];
+               }
+            }
+            catch
+            {
+                textBoxInfo.Text = "You didn't choose the object.\r\n";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                if (CheckInput())
+                {
+                    FieldList.Clear();
+                    FieldList.Add(comboBox1.GetItemText(comboBox1.SelectedItem));
+                    for (i = 1; i <= TextBoxList.Count; i++)
+                        if (TextBoxList[i - 1].Text != "")
+                            FieldList.Add(TextBoxList[i - 1].Text);
+                    DecorationList[listBox1.SelectedIndex].Object = comboBox1.GetItemText(comboBox1.SelectedItem);
+                    DecorationList[listBox1.SelectedIndex].SetValues(FieldList);
+                    flagEdit = true;
+                    listBox1.Items[listBox1.SelectedIndex] = "Object =" + DecorationList[listBox1.SelectedIndex].Object + "   Name ="
+                        + DecorationList[listBox1.SelectedIndex].Name + "   Weight =" + DecorationList[listBox1.SelectedIndex].Weight
+                        + "   Material =" + DecorationList[listBox1.SelectedIndex].Material;
+                    flagEdit = false;
+                    textBoxInfo.Text = "The object edited.\r\n";
+                }
+                else
+                {
+                    textBoxInfo.Text = "Check the input values\r\n";
+                }
+            }
+            else
+            {
+                textBoxInfo.Text = "Add the object to list.\r\n";
+            }
+        }
+    }
+}
